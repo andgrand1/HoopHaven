ba35892349b5c6aa266c5bcc49d5491238ebba83
Brayden Smith <brayden.smithzimmerman@gmail.com>
2023-11-20 19:01:31 -0600

    typedefs file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,12 @@
-const { ApolloServer, gql } = require('@apollo/server');
+const { ApolloServer, gql } = require("@apollo/server");
 
 const typeDefs = gql`
   type User {
-    _id: ID!
+    id: ID!
     username: String!
     email: String!
-    # Add other user-related fields as needed
-    cart: [Listing] # Assuming a user has a cart that contains listings
+    cart: [CartItem]!
+    listings: [Listing]!
   }
 
   type Listing {
@@ -20,6 +20,40 @@ const typeDefs = gql`
     active: Boolean!
     pictures: [String]!
     createdBy: User!
+  }
+
+  type Query {
+    getlisting(id: ID!): Listing
+    getUsers: [User]!
+    getListings: [Listing]!
+    getUserCart(userId: ID!): [CartItem]!
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!): User!
+    createListing(
+      title: String!
+      description: String!
+      price: Float!
+      size: String!
+      gender: String!
+      category: String!
+      active: Boolean!
+      pictures: [String]!
+    ): Listing
+    editListing(
+      id: ID!
+      title: String
+      description: String
+      price: Float
+      size: String
+      gender: String
+      category: String
+      active: Boolean
+      pictures: [String]
+    ): Listing
+    addToCart(userId: ID!, listingId: ID!, quantity: Int!): User!
+    removeFromCart(userId: ID!, cartItemId: ID!): User!
   }
 `;
 
