836a60b5d5e2314b8c0d901999aae3a0005ed818
Brayden Smith <brayden.smithzimmerman@gmail.com>
2023-11-20 20:38:41 -0600

    more work on mutations and queries

--- client/utils/mutations.js
+++ client/utils/mutations.js
@@ -1,5 +1,65 @@
 import { gql } from '@apollo/client';
 
+export const CREATE_USER = gql`
+  mutation CreateUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      id
+      username
+      email
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+      user {
+        id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const CREATE_LISTING = gql`
+  mutation CreateListing(
+    $title: String!
+    $description: String!
+    $price: Float!
+    $size: String!
+    $gender: String!
+    $category: String!
+    $active: Boolean!
+    $pictures: [String]!
+  ) {
+    createListing(
+      title: $title
+      description: $description
+      price: $price
+      size: $size
+      gender: $gender
+      category: $category
+      active: $active
+      pictures: $pictures
+    ) {
+      id
+      title
+      description
+      price
+      size
+      gender
+      category
+      active
+      pictures
+      createdBy {
+        id
+        username
+      }
+    }
+  }
+`;
 export const EDIT_LISTING_MUTATION = gql`
   mutation EditListing(
     $id: ID!
@@ -23,7 +83,7 @@ export const EDIT_LISTING_MUTATION = gql`
       pictures: $pictures
       active: $active
     ) {
-      _id
+      id
       title
       description
       price
@@ -33,8 +93,50 @@ export const EDIT_LISTING_MUTATION = gql`
       pictures
       active
       createdBy {
-        _id
+        id
         username
+      }
+    }
+  }
+`;
+
+export const ADD_TO_CART = gql`
+  mutation AddToCart($userId: ID!, $listingId: ID!, $quantity: Int!) {
+    addToCart(userId: $userId, listingId: $listingId, quantity: $quantity) {
+      id
+      cart {
+        listing {
+          id
+          title
+          description
+          price
+          size
+          gender
+          category
+          active
+          pictures
+        }
+      }
+    }
+  }
+`;
+
+export const REMOVE_FROM_CART = gql`
+  mutation RemoveFromCart($userId: ID!, $cartItemId: ID!) {
+    removeFromCart(userId: $userId, cartItemId: $cartItemId) {
+      id
+      cart {
+        listing {
+          id
+          title
+          description
+          price
+          size
+          gender
+          category
+          active
+          pictures
+        }
       }
     }
   }
